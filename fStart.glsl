7b4deecafb5ab3abdd0f0c0cef630fb7c412560f
jknlsn <j.n@live.com.au>
2017-05-27 15:33:30 +0800

    Attempted G, not working perfectly

--- fStart.glsl
+++ fStart.glsl
@@ -1,77 +1,52 @@
-varying vec4 color;
+// varying vec4 color;
+// varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+//
+// uniform sampler2D texture;
+//
+// void main()
+// {
+//     gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
+// }
+
+
+// removed
+// varying vec4 color;
+
+// added
+varying vec3 N;
+varying vec3 L;
+varying vec3 E;
+varying vec3 H;
+vec4 color;
+// kept
 varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+
+// Added
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 uniform sampler2D texture;
 
 void main()
 {
+    // Compute terms in the illumination equation
+    vec4 ambient = vec4(AmbientProduct,1.0);
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec4  diffuse = Kd * vec4(DiffuseProduct,1.0);
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec4  specular = Ks * vec4(SpecularProduct,1.0);
+
+    if (dot(L, N) < 0.0 ) {
+      specular = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+
+    // globalAmbient is independent of distance from the light source
+    vec4 globalAmbient = vec4(0.1, 0.1, 0.1, 1.0);
+    color = globalAmbient + ambient + diffuse + specular;
+    color.a = 1.0;
     gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
 }
-
-// varying vec3 fN;
-// varying vec3 fL1;
-// // varying vec3 fL2;
-// varying vec3 fE;
-// varying vec2 texCoord;
-//
-// uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
-// uniform mat4 ModelView;
-// uniform vec4 LightPosition1;
-// // uniform vec4 LightPosition2;
-// uniform float Shininess;
-// uniform sampler2D texture;
-//
-// void main()
-// {
-//     vec3 N = normalize(fN);
-//     vec3 E = normalize(fE);
-//     vec3 L1 = normalize(fL1-fE);
-//     // vec3 L2 = normalize(fL2-fE);
-//
-//     vec3 H1 = normalize(L1+E);
-//     // vec3 H2 = normalize(L2+E);
-//     vec4 ambient = vec4(AmbientProduct,1.0);
-//
-//     float Kd1 = max(dot(L1, N), 0.0);
-//     // float Kd2 = max(dot(L2, N), 0.0);
-//     vec4 diffuse1 = Kd1*vec4(DiffuseProduct,1.0);
-//     // vec4 diffuse2 = Kd2*vec4(DiffuseProduct,1.0);
-//
-//     float Ks1 = pow(max(dot(N, H1), 0.0), Shininess);
-//     // float Ks2 = pow(max(dot(N, H2), 0.0), Shininess);
-//     vec4 specular1 = Ks1*vec4(SpecularProduct,1.0);
-//     // vec4 specular2 = Ks2*vec4(SpecularProduct,1.0);
-//
-//     if(dot(L1,N) < 0.0)
-//     {
-//         specular1 = vec4(0.0,0.0,0.0,1.0);
-//     }
-//
-//     // if(dot(L2,N) < 0.0)
-//     // {
-//     //     specular2 = vec4(0.0,0.0,0.0,1.0);
-//     // }
-//
-//     //adjust rgb values via a weighted average following luminosity standards for human vision, to create equivalent grayscale light for specular element.
-//     float temp1 = 0.2989 * specular1.x + 0.5870 * specular1.y + 0.1140 * specular1.z;
-//     specular1.xyz = vec3(temp1,temp1,temp1);
-//
-//     // float temp2 = 0.2989 * specular2.x + 0.5870 * specular2.y + 0.1140 * specular2.z;
-//     // specular2.xyz = vec3(temp2,temp2,temp2);
-//
-//     // globalAmbient is independent of distance from the light source
-//     vec4 globalAmbient = vec4(0.1, 0.1, 0.1, 1.0);
-//
-//     //brightness fade by distance, doesn't affect global ambient
-//     float atten = (0.3*length(fL1)+0.2*length(fL1)*length(fL1));
-//     // float atten2 = (0.3*length(fL2)+0.2*length(fL2)*length(fL2));
-//     //float atten = 0.5;
-//
-//     //vec3 Light1 = diffuse1.xyz + specular1.xyz;
-//     //vec3 Light2 = diffuse2.xyz + specular2.xyz;
-//     vec4 color = ambient+(diffuse1 + specular1)/atten + globalAmbient;
-//     // color = color + (diffuse2 + specular2)/atten2;
-//     color.a = 1.0;
-//     gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
-//
-// }
--- vStart.glsl
+++ vStart.glsl
@@ -1,18 +1,87 @@
+// attribute vec3 vPosition;
+// attribute vec3 vNormal;
+// attribute vec2 vTexCoord;
+//
+// varying vec2 texCoord;
+// varying vec4 color;
+//
+// uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+// uniform mat4 ModelView;
+// uniform mat4 Projection;
+// uniform vec4 LightPosition;
+// uniform float Shininess;
+//
+// void main()
+// {
+//     vec4 vpos = vec4(vPosition, 1.0);
+//
+//     // Transform vertex position into eye coordinates
+//     vec3 pos = (ModelView * vpos).xyz;
+//
+//
+//     // The vector to the light from the vertex
+//     vec3 Lvec = LightPosition.xyz - pos;
+//
+//     // Unit direction vectors for Blinn-Phong shading calculation
+//     vec3 L = normalize( Lvec );   // Direction to the light source
+//     vec3 E = normalize( -pos );   // Direction to the eye/camera
+//     vec3 H = normalize( L + E );  // Halfway vector
+//
+//     // Transform vertex normal into eye coordinates (assumes scaling
+//     // is uniform across dimensions)
+//     vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+//
+//     // Compute terms in the illumination equation
+//     vec3 ambient = AmbientProduct;
+//
+//     float Kd = max( dot(L, N), 0.0 );
+//     vec3  diffuse = Kd*DiffuseProduct;
+//
+//     float Ks = pow( max(dot(N, H), 0.0), Shininess );
+//     vec3  specular = Ks * SpecularProduct;
+//
+//     if (dot(L, N) < 0.0 ) {
+// 	specular = vec3(0.0, 0.0, 0.0);
+//     }
+//
+//     // globalAmbient is independent of distance from the light source
+//     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+//     color.rgb = globalAmbient  + ambient + diffuse + specular;
+//     color.a = 1.0;
+//
+//     gl_Position = Projection * ModelView * vpos;
+//     texCoord = vTexCoord;
+// }
+
+
 attribute vec3 vPosition;
 attribute vec3 vNormal;
 attribute vec2 vTexCoord;
 
-varying vec2 texCoord;
-varying vec4 color;
+// removed
+// varying vec4 color;
 
-uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+// added
+varying vec3 N;
+varying vec3 E;
+varying vec3 L;
+varying vec3 H;
+
+//  kept
+varying vec2 texCoord;
+
+// Keep
 uniform mat4 ModelView;
-uniform mat4 Projection;
 uniform vec4 LightPosition;
-uniform float Shininess;
+uniform mat4 Projection;
+
+// Lose
+// uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+// uniform float Shininess;
 
 void main()
 {
+
     vec4 vpos = vec4(vPosition, 1.0);
 
     // Transform vertex position into eye coordinates
@@ -22,85 +91,38 @@ void main()
     // The vector to the light from the vertex
     vec3 Lvec = LightPosition.xyz - pos;
 
-    // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
+    // // Unit direction vectors for Blinn-Phong shading calculation
+    // vec3 L = normalize( Lvec );   // Direction to the light source
+    // vec3 E = normalize( -pos );   // Direction to the eye/camera
+    // vec3 H = normalize( L + E );  // Halfway vector
+
+    L = normalize( Lvec );   // Direction to the light source
+    E = normalize( -pos );   // Direction to the eye/camera
+    H = normalize( L + E );  // Halfway vector
 
     // Transform vertex normal into eye coordinates (assumes scaling
     // is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    // vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
 
-    // Compute terms in the illumination equation
-    vec3 ambient = AmbientProduct;
-
-    float Kd = max( dot(L, N), 0.0 );
-    vec3  diffuse = Kd*DiffuseProduct;
-
-    float Ks = pow( max(dot(N, H), 0.0), Shininess );
-    vec3  specular = Ks * SpecularProduct;
-
-    if (dot(L, N) < 0.0 ) {
-	specular = vec3(0.0, 0.0, 0.0);
-    }
-
-    // globalAmbient is independent of distance from the light source
-    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    color.rgb = globalAmbient  + ambient + diffuse + specular;
-    color.a = 1.0;
+  //   // Compute terms in the illumination equation
+  //   vec3 ambient = AmbientProduct;
+  //
+  //   float Kd = max( dot(L, N), 0.0 );
+  //   vec3  diffuse = Kd*DiffuseProduct;
+  //
+  //   float Ks = pow( max(dot(N, H), 0.0), Shininess );
+  //   vec3  specular = Ks * SpecularProduct;
+  //
+  //   if (dot(L, N) < 0.0 ) {
+	// specular = vec3(0.0, 0.0, 0.0);
+  //   }
+  //
+  //   // globalAmbient is independent of distance from the light source
+  //   vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+  //   color.rgb = globalAmbient  + ambient + diffuse + specular;
+  //   color.a = 1.0;
 
     gl_Position = Projection * ModelView * vpos;
     texCoord = vTexCoord;
 }
-
-
-// attribute vec3 vPosition;
-// attribute vec3 vNormal;
-// attribute vec2 vTexCoord;
-// // attribute vec4 boneIDs;
-// // attribute vec4 boneWeights;
-//
-// varying vec3 fN;
-// varying vec3 fE;
-// varying vec3 fL1;
-// // varying vec3 fL2;
-// varying vec2 texCoord;
-//
-// uniform mat4 ModelView;
-// uniform vec4 LightPosition1;
-// uniform vec4 LightPosition2;
-// uniform mat4 Projection;
-// // uniform mat4 boneTransforms[64];
-//
-// void main()
-// {
-//     // mat4 boneTransform = boneWeights[0]*boneTransforms[int(boneIDs[0])]+
-//     //                     boneWeights[1]*boneTransforms[int(boneIDs[1])]+
-//     //                     boneWeights[2]*boneTransforms[int(boneIDs[2])]+
-//     //                     boneWeights[3]*boneTransforms[int(boneIDs[3])];
-//
-//     vec4 vpos = vec4(vPosition,1.0);
-//     // vec4 vpos = boneTransform * vec4(vPosition,1.0);
-//
-//     // vec3 pos = (boneTransform * ModelView * vpos).xyz;
-//     vec3 pos = (ModelView * vpos).xyz;
-//
-//     vec4 Normal = vec4(vNormal,1.0);
-//     // vec4 Normal = boneTransform * vec4(vNormal,1.0);
-//
-//     fN = (ModelView * Normal).xyz;
-//     fE = -pos;
-//     fL1 = LightPosition1.xyz;
-//     // fL2 = LightPosition2.xyz;
-//
-//     if(LightPosition1.w != 0.0){
-//         fL1 = LightPosition1.xyz - pos;
-//     }
-//
-//     // if(LightPosition2.w != 0.0){
-//     //     fL2 = LightPosition2.xyz - pos;
-//     // }
-//
-//     gl_Position = Projection * ModelView * vpos;
-//     texCoord = vTexCoord;
-// }
