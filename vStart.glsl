e002a1d15fb234737a2afb75c2a0459bc06e0ee6
jknlsn <j.n@live.com.au>
2017-05-30 19:12:27 +0800

    first addition of bonetransforms and modifying vpos and vnorm

--- vStart.glsl
+++ vStart.glsl
@@ -2,6 +2,10 @@ attribute vec3 vPosition;
 attribute vec3 vNormal;
 attribute vec2 vTexCoord;
 
+attribute vec4 boneIDs;
+attribute vec4 boneWeights;
+uniform mat4 boneTransforms[64];
+
 varying vec3 N;
 varying vec3 pos;
 varying vec3 Lvec1;
@@ -16,7 +20,15 @@ uniform mat4 Projection;
 void main()
 {
 
-    vec4 vpos = vec4(vPosition, 1.0);
+    mat4 boneTransform = boneWeights[0] * boneTransforms[int(boneIDs[0])] +
+                         boneWeights[1] * boneTransforms[int(boneIDs[1])] +
+                         boneWeights[2] * boneTransforms[int(boneIDs[2])] +
+                         boneWeights[3] * boneTransforms[int(boneIDs[3])] ;
+
+    vec4 vpos = boneTransform * vec4(vPosition,1.0);
+    vec4 vnorm = boneTransform * vec4(vNormal,0.0);
+
+    // vec4 vpos = vec4(vPosition, 1.0);
 
     // Transform vertex position into eye coordinates
     pos = (ModelView * vpos).xyz;
@@ -29,7 +41,7 @@ void main()
     // // Transform vertex normal into eye coordinates (assumes scaling
     // // is uniform across dimensions)
     // // vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
-    N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    N = normalize( (ModelView * vnorm).xyz );
 
     gl_Position = Projection * ModelView * vpos;
     texCoord = vTexCoord;
